7bcb1c68a699500f111ab9854c186f76361d4664
Julien Maurel <julien-maurel@hotmail.com>
2024-11-04 17:34:58 +0000

    state machine beginning

--- game/Worker.cpp
+++ game/Worker.cpp
@@ -3,10 +3,12 @@
 
 CWorker::CWorker(Sint16 x, Sint16 y, CGraphics* pGraphicsLeft, CGraphics* pGraphicsRight, Uint32 time) : CSprite(x, y, 0, 0, time)
 {
+	state = IDLE;
 	LoadAnimation(pGraphicsLeft, "idleL", CSprite::Sheet(6, 6).Row(5).From(0).To(5));
 	LoadAnimation(pGraphicsRight, "idleR", CSprite::Sheet(6, 6).Row(5).From(0).To(5));
 	LoadAnimation(pGraphicsLeft, "walkL", CSprite::Sheet(6, 6).Row(4).From(0).To(5));
 	LoadAnimation(pGraphicsRight, "walkR", CSprite::Sheet(6, 6).Row(4).From(0).To(5));
+	LoadAnimation(pGraphicsRight, "cutting", CSprite::Sheet(6, 6).Row(2).From(0).To(5));
 	SetAnimation("idleR");
 }
 
@@ -19,7 +21,50 @@ int *CWorker::getCell(void) {
 	return cell;
 }
 
+void CWorker::ChangeState(STATE newState)
+{
+	if (newState == state)
+		return;		// No state change
+
+	state = newState;
+
+	switch (state)
+	{
+	case IDLE:
+		SetVelocity(0, 0);
+		SetAnimation("idleR");
+		break;
+	case WALKING:
+		break;
+	case CUTTING:
+		SetVelocity(0, 0);
+		SetAnimation("cutting");
+		break;
+	}
+}
+
 void CWorker::OnUpdate(Uint32 time, Uint32 deltaTime)
 {
+	// State dependant actions
+	switch (state)
+	{
+	case IDLE:
+		break;
+	case CUTTING:
+		break;
+	case WALKING:
+		break;
+	}
+
+	// Transitions
+	switch (state)
+	{
+	case IDLE:
+		break;
+	case CUTTING:
+		break;
+	case WALKING:
+		break;
+	}
 	CSprite::OnUpdate(time, deltaTime);
 }
--- game/Worker.h
+++ game/Worker.h
@@ -4,10 +4,18 @@ class CWorker : public CSprite
 {
 
 public:
+	enum STATE { NONE, IDLE, WALKING, CUTTING };
+
+private:
+	STATE state;
+
+public:
 	CWorker(Sint16 x, Sint16 y, CGraphics* pGraphicsLeft, CGraphics* pGraphicsRight, Uint32 time);
 	~CWorker(void);
 
 	int* getCell(void);
+	void ChangeState(STATE newState);
+	STATE GetState() { return state; }
 
 	void OnUpdate(Uint32 time, Uint32 deltaTime);
 };
